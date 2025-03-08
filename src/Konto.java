6de5356aed4d32f6308cb98bf779225cf5637ec6
Mateusz Kowal <mateusz.pawel.kowal@gmail.com>
2025-03-08 19:38:29 +0100

    Konto

--- src/Konto.java
+++ src/Konto.java
@@ -2,13 +2,14 @@ public class Konto {
     private final int numerKonta;
     protected double saldo;
 
-    public Konto(int numerKonta) {
-        this(numerKonta, 0.0);
+    public Konto(int numer) {
+        this.numerKonta = numer;
+        this.saldo = 0;
     }
 
-    public Konto(int numerKonta, double saldo) {
-        this.numerKonta = numerKonta;
-        this.saldo = saldo;
+    public Konto(int numer, double kwota) {
+        this.numerKonta = numer;
+        this.saldo = kwota;
     }
 
     public void wplata(double kwota) {
@@ -18,7 +19,7 @@ public class Konto {
     }
 
     public void wyplata(double kwota) {
-        if (kwota > 0 && kwota <= saldo) {
+        if (kwota <= saldo) {
             saldo -= kwota;
         } else {
             System.out.println("Brak środków na koncie!");
