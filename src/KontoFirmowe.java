0d436751b2b38418d863cb8b0bf3009d99371b38
Mateusz Kowal <mateusz.pawel.kowal@gmail.com>
2025-03-08 19:55:24 +0100

    Konto firmowe

---
+++ src/KontoFirmowe.java
@@ -0,0 +1,18 @@
+public class KontoFirmowe extends Konto{
+    private double limitKredytowy;
+
+    public KontoFirmowe(int numerKonta, double saldo, double limitKredytowy) {
+        super(numerKonta, saldo);
+        this.limitKredytowy = limitKredytowy;
+    }
+
+    @Override
+    public void wyplata(double kwota) {
+        if (kwota <= saldo + limitKredytowy) {
+            saldo -= kwota;
+            System.out.println("Wypłacono z konta firmowego: " + kwota);
+        } else {
+            System.out.println("Przekroczono limit kredytowy!");
+        }
+    }
+}
